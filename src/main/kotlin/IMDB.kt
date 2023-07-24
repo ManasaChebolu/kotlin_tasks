d5d0f4f64807964e835586736684c93c99d28667
manasa.ch <manasa.c@marketsimplified.com>
2023-07-24 13:02:34 +0530

    commit!!

--- src/main/kotlin/IMDB.kt
+++ src/main/kotlin/IMDB.kt
@@ -25,17 +25,17 @@ class IMDB {
     }
 }
 fun main() {
-    val imdb=IMDB()
-    movieList.add(Movie("Mem Famous",2023,8.2,"Telugu"))
-    movieList.add(Movie("HIT",2020,7.6,"Telugu"))
-    movieList.add(Movie("Vada chennai",2018,8.4,"Tamil"))
-    movieList.add(Movie("the meg",2002,5.6,"English"))
-    movieList.add(Movie("Shang chi",2021,8.4,"English"))
-    val search= readln()?.trim()
-    if(search!=null) {
+    val imdb = IMDB()
+    movieList.add(Movie("Mem Famous", 2023, 8.2, "Telugu"))
+    movieList.add(Movie("HIT", 2020, 7.6, "Telugu"))
+    movieList.add(Movie("Vada chennai", 2018, 8.4, "Tamil"))
+    movieList.add(Movie("the meg", 2002, 5.6, "English"))
+    movieList.add(Movie("Shang chi", 2021, 8.4, "English"))
+    val search = readln()?.trim()
+    if (search != null) {
         val searchList = imdb.search(search)
         searchList.forEach { println(it.title) }
     }
-    val ratingList =imdb.movieRating(scan.nextDouble())
-    ratingList.forEach{println(it.title)}
+    val ratingList = imdb.movieRating(scan.nextDouble())
+    ratingList.forEach { println(it.title) }
 }
